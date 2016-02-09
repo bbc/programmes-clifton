e4e2770237b52641e518b39fee67a1151b10eaab
Ben Scott <ben.scott@bbc.co.uk>
2016-02-08 20:39:50 -0800

    Remove migrations bundle as clifton doesnt manage the DB

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new BBC\CliftonBundle\CliftonBundle(),
