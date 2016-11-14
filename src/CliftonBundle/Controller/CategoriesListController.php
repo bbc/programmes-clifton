e82646bd6d966376fdbf4d93d0905984510824b1
Felipe Araujo <felipe.peixoto.araujo@gmail.com>
2016-11-14 16:55:18 +0000

    Updated  pages-service and changed the way we use the categories service

--- src/CliftonBundle/Controller/CategoriesListController.php
+++ src/CliftonBundle/Controller/CategoriesListController.php
@@ -13,11 +13,19 @@ class CategoriesListController extends BaseApsController
     public function panBbcCategoriesListAction(Request $request, string $category): JsonResponse
     {
         /** @var Format[]|Genre[] $categories */
-        $categories = $this->get('pps.categories_service')->findUsedByType($category);
+        if ($category === 'genres') {
+            $categories = $this->get('pps.categories_service')->findUsedGenres($category);
+        } elseif ($category === 'formats') {
+            $categories = $this->get('pps.categories_service')->findUsedFormats($category);
+        } else {
+            // This shouldn't really happen as the route only matches genres or formats, but better safe than sorry
+            throw $this->createNotFoundException(sprintf("'%s' is not a valid category type", $category));
+        }
 
         $topLevelCategories = [];
         $secondLevelCategories = [];
 
+        // Separate between categories and subcategories
         foreach ($categories as $category) {
             if ($category instanceof Format || is_null($category->getParent())) {
                 $topLevelCategories[] = $category;
@@ -28,15 +36,18 @@ class CategoriesListController extends BaseApsController
 
         $mappedCategories = [];
 
+        // The mapper is only called for top level categories, subcategories are passed as an additional parameter
         foreach ($topLevelCategories as $topLevelCategory) {
             $subcategories = [];
 
+            // Separate a category's subcategories
             foreach ($secondLevelCategories as $subcategory) {
                 if ($subcategory->getParent()->getId() === $topLevelCategory->getId()) {
                     $subcategories[] = $subcategory;
                 }
             }
 
+            // Map the category and its subcategories
             $mappedCategories[] = $this->mapSingleApsObject(
                 new CategoryItemMapper(),
                 $topLevelCategory,
