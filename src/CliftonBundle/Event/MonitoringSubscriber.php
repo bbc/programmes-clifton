4a9b78c7e4fc35d0d961597aad6d9ebe9386722c
Ben Scott <ben.scott@bbc.co.uk>
2016-12-01 14:20:09 +0000

    Add an app-access log
    This contains the controller class name and action that is triggered,
    along with the time in millisecods that the request took to render
    (excluding symfony bootup time). This allows us to monitor response
    times on a per-route basis - for instance the findByPid route takes X on
    average, while a category list takes Y on average. Thus we can identify
    speed regressions within particular routes rather than across the whole
    application.

---
+++ src/CliftonBundle/Event/MonitoringSubscriber.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace BBC\CliftonBundle\Event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+use Symfony\Component\Stopwatch\Stopwatch;
+use Psr\Log\LoggerInterface;
+
+class MonitoringSubscriber implements EventSubscriberInterface
+{
+    const REQUEST_TIMER = 'aps.request_time';
+
+    private $logger;
+    private $stopwatch;
+
+    public function __construct(LoggerInterface $logger, Stopwatch $stopwatch)
+    {
+        $this->logger = $logger;
+        $this->stopwatch = $stopwatch;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            // Start timer
+            KernelEvents::REQUEST => [['requestStart', 512]],
+            // Stop timer and log the duration
+            KernelEvents::TERMINATE => [['logTiming', -512]],
+        ];
+    }
+
+    public function requestStart(KernelEvent $event) {
+        if ($event->isMasterRequest()) {
+            $this->stopwatch->start(self::REQUEST_TIMER, 'section');
+        }
+    }
+
+    public function logTiming(KernelEvent $event)
+    {
+        if ($event->isMasterRequest()) {
+            $this->stopwatch->stop(self::REQUEST_TIMER);
+        }
+
+        $controllerAction = $event->getRequest()->attributes->get('_controller');
+
+        if (!$controllerAction || strpos($controllerAction, 'BBC\CliftonBundle\Controller') === false) {
+            return;
+        }
+
+        $controllerPeriod = $this->getControllerPeriod();
+        if ($controllerPeriod) {
+            $this->logger->info('CONTROLLER {0} {1}',  [$controllerAction, $controllerPeriod]);
+        }
+    }
+
+    private function getControllerPeriod()
+    {
+        foreach ($this->stopwatch->getSections() as $section) {
+            $event = $section->getEvents()[self::REQUEST_TIMER] ?? null;
+            if ($event) {
+                return $event->getDuration();
+            }
+        }
+
+        return null;
+    }
+}
