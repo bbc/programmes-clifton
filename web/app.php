691213dad8cc5d58bb3ecfc33e2d487d32bd6fe5
Ben Scott <ben.scott@bbc.co.uk>
2016-12-07 11:07:00 +0000

    Update to follow symfony-standard 3.2 conventions
    Removed some unneeded files in the web folder

--- web/app.php
+++ web/app.php
@@ -2,24 +2,16 @@
 declare(strict_types=1); // php 7 strict mode
 use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @var Composer\Autoload\ClassLoader
- */
+/** @var /Composer\Autoload\ClassLoader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../app/bootstrap.php.cache';
-
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../app/bootstrap.php.cache';
+}
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
