501154d73809e76c8cee3833f439f0626df202ce
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2017-05-02 12:35:44 +0100

    Do some dodgy and possibly illegal things to attempt to catch APCU caches going wrong on our status page.

--- tests/Controller/StatusControllerTest.php
+++ tests/Controller/StatusControllerTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\BBC\CliftonBundle\Controller;
 
+use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
+use BBC\ProgrammesPagesService\Service\ProgrammesService;
+use Doctrine\DBAL\ConnectionException;
+use Doctrine\DBAL\DBALException;
 use Tests\BBC\CliftonBundle\BaseWebTestCase;
 
 class StatusControllerTest extends BaseWebTestCase
@@ -20,9 +24,52 @@ class StatusControllerTest extends BaseWebTestCase
         $client = static::createClient([], [
             'HTTP_USER_AGENT' => 'ELB-HealthChecker/1.0',
         ]);
-        $crawler = $client->request('GET', '/status');
+        $client->request('GET', '/status');
 
         $this->assertResponseStatusCode($client, 200);
         $this->assertEquals('OK', $client->getResponse()->getContent());
+    }
+
+    public function testNonConnectionDBErrorFromElb()
+    {
+        $client = static::createClient([], [
+            'HTTP_USER_AGENT' => 'ELB-HealthChecker/1.0',
+        ]);
+        // Create mock service to throw exception and inject into container
+        $mockService = $this->createMockProgrammesService();
+        $mockService->expects($this->once())
+            ->method('findByPidFull')
+            ->with(new Pid('b006m86d'))
+            ->willThrowException(new DBALException("Something bad happened."));
+        $client->getContainer()->set('pps.programmes_service', $mockService);
+
+        $client->request('GET', '/status');
+
+        $this->assertResponseStatusCode($client, 500);
+        $this->assertEquals('ERROR', $client->getResponse()->getContent());
+    }
+
+    public function testConnectionDBErrorFromElb()
+    {
+        $client = static::createClient([], [
+            'HTTP_USER_AGENT' => 'ELB-HealthChecker/1.0',
+        ]);
+        // Create mock service to throw exception and inject into container
+        $mockService = $this->createMockProgrammesService();
+        $mockService->expects($this->once())
+            ->method('findByPidFull')
+            ->with(new Pid('b006m86d'))
+            ->willThrowException(new ConnectionException("Cannot Connect."));
+        $client->getContainer()->set('pps.programmes_service', $mockService);
+
+        $client->request('GET', '/status');
+
+        $this->assertResponseStatusCode($client, 200);
+        $this->assertEquals('OK', $client->getResponse()->getContent());
+    }
+
+    private function createMockProgrammesService()
+    {
+        return $this->createMock(ProgrammesService::class);
     }
 }
