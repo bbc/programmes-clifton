0991f80fbbe1a81e5a8e69a156224bcc2c6be9ab
Bjorn Twachtmann <BjornTwachtmann@users.noreply.github.com>
2019-05-23 15:01:23 +0100

    Update symfony and fix deprecations (#17)

--- tests/BaseWebTestCase.php
+++ tests/BaseWebTestCase.php
@@ -24,6 +24,31 @@ abstract class BaseWebTestCase extends WebTestCase
         $this->assertEquals($expectedLocation, $client->getResponse()->headers->get('location'));
     }
 
+    /**
+     * Taken from Symfony/Bundle/FrameworkBundle/Test/KernelTestCase (this
+     * class's grandparent), as the method in
+     * Liip\FunctionalTestBundle\Test\WebTestCase (this class's parent) is
+     * currently using the KERNEL_DIR param that was deprecated in Symfony 3.4.
+     *
+     * Delete me once Liip\FunctionalTestBundle has been updated to support
+     * Symfony 3.4 without any deprecation notices.
+     *
+     * @return string The Kernel class name
+     *
+     * @throws \RuntimeException
+     * @throws \LogicException
+     */
+    protected static function getKernelClass()
+    {
+        if (!isset($_SERVER['KERNEL_CLASS']) && !isset($_ENV['KERNEL_CLASS'])) {
+            throw new \LogicException(sprintf('You must set the KERNEL_CLASS environment variable to the fully-qualified class name of your Kernel in phpunit.xml / phpunit.xml.dist or override the %1$s::createKernel() or %1$s::getKernelClass() method.', static::class));
+        }
+        if (!class_exists($class = $_ENV['KERNEL_CLASS'] ?? $_SERVER['KERNEL_CLASS'])) {
+            throw new \RuntimeException(sprintf('Class "%s" doesn\'t exist or cannot be autoloaded. Check that the KERNEL_CLASS value in phpunit.xml matches the fully-qualified class name of your Kernel or override the %s::createKernel() method.', $class, static::class));
+        }
+        return $class;
+    }
+
     protected function loadFixtures(array $fixtureNames, $omName = null, $registryName = 'doctrine', $purgeMode = null)
     {
         $classNames = array();
